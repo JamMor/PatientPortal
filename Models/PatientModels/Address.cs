0e2ed7c474315776766135074ee5dbc19b1f68e2
JamMor <JamMor@gmail.com>
2021-10-17 11:58:12 -0700

    MessageLink on Patient Creation w Optional Address

--- Models/PatientModels/Address.cs
+++ Models/PatientModels/Address.cs
@@ -9,19 +9,14 @@ namespace PatientPortal.Models
         [ForeignKey("Patient")]
         public int AddressId { get; set; }
         
-        [Required]
         [Display(Name = "Street Address")]
         public string StreetAddress { get; set; }
-        [Required]
         public string City { get; set; }
-        [Required]
         public string State { get; set; }
-        [Required]
         [Display(Name = "ZIP Code")]
         [DataType(DataType.PostalCode)]
-        public int ZipCode { get; set; }
+        public int? ZipCode { get; set; }
 
-        [Required]
         public int PatientId { get; set; }
         public Patient Patient { get; set; }
     }
