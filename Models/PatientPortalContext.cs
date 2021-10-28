e78ad9550f5be4fa003afdfd506af3936295ea3a
JamMor <JamMor@gmail.com>
2021-10-28 01:54:36 -0700

    Added comment to prettify Context

--- Models/PatientPortalContext.cs
+++ Models/PatientPortalContext.cs
@@ -15,6 +15,8 @@ namespace PatientPortal.Models
         public DbSet<TestResult> TestResults { get;set; }
         public DbSet<TestHealthIssueAssociation> TestHealthIssueAssociations { get;set; }
         public DbSet<VisitHealthIssueAssociation> VisitHealthIssueAssociations { get;set; }
+
+        //Messaging
         public DbSet<MessagingLink> MessagingLinks { get;set; }
         public DbSet<Conversation> Conversations { get;set; }
         public DbSet<ConversationParticipant> ConversationParticipants { get;set; }
