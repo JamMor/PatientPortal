907041efda513d49bc80f0a7ce76352e6c6e563a
JamMor <JamMor@gmail.com>
2021-11-13 04:53:38 -0800

    Added Messaging Link On Staff and Patient Info

--- ViewModels/PatientInfoHeaderView.cs
+++ ViewModels/PatientInfoHeaderView.cs
@@ -7,13 +7,10 @@ namespace PatientPortal.Models
     public class PatientHeaderInfoView
     {
         public int CurrentPatientId { get; set; }
-        
+        public int CurrentPatientLinkId { get; set; }
         public string CurrentPatientFirstName { get; set; }
-        
         public string CurrentPatientLastName { get; set; }
-
         public string CurrentPatientSSN { get; set; }
-        
         public DateTime CurrentPatientDOB { get; set; }
         public int CurrentPatientAge { get; set; }
         public DateTime CurrentPatientCreatedOn { get; set; }
@@ -22,7 +19,7 @@ namespace PatientPortal.Models
         {
             get => $"{CurrentPatientFirstName} {CurrentPatientLastName}";
         }
-        
+
         public string CurrentPatientFullNameReverse
         {
             get => $"{CurrentPatientLastName}, {CurrentPatientFirstName}";
