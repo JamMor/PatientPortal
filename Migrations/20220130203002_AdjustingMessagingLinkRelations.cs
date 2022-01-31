aab35469871421b73dbcf20d7cc71a42d3d24073
JamMor <JamMor@gmail.com>
2022-01-30 23:08:13 -0800

    Changed ForeignKey Annotation of MessageLink, though same relations

---
+++ Migrations/20220130203002_AdjustingMessagingLinkRelations.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PatientPortal.Migrations
+{
+    public partial class AdjustingMessagingLinkRelations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
