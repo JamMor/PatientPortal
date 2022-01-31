5045b137e95e10e9b916b422c97881da8e13fdce
JamMor <JamMor@gmail.com>
2022-01-31 04:06:04 -0800

    Created Paged List Extension Method

---
+++ Extensions/PaginationAndSortingExtensions.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Linq;
+using PatientPortal.Models;
+
+namespace PatientPortal.Extensions
+{
+    public static class PaginationExtensions
+    {
+        public static List<T> ToPagedList<T>(this IQueryable<T> query, int resultsPerPage, int currentPage)
+        {
+            return query
+                .Skip(resultsPerPage*(currentPage-1))
+                .Take(resultsPerPage)
+                .ToList();
+        }
+    }
+}
