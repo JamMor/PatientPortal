c3bc147322e755bde702263052a14ee618b9b4a2
JamMor <JamMor@gmail.com>
2022-02-01 23:04:21 -0800

    Extensions to Group Services for Startup Injection

---
+++ Configuration/ConfigureCoreServices.cs
@@ -0,0 +1,22 @@
+using Microsoft.Extensions.DependencyInjection;
+using PatientPortal.Interfaces;
+using PatientPortal.Services;
+
+namespace PatientPortal.Configuration
+{
+    public static class ConfigureCoreServices
+    {
+        public static IServiceCollection AddCoreServices(this IServiceCollection services)
+        {
+            services.AddTransient<IStaffService, StaffService>();
+            services.AddTransient<IPatientService, PatientService>();
+            services.AddTransient<IPatientStaffConnectionService, PatientStaffConnectionService>();
+            services.AddTransient<IHealthIssueService, HealthIssueService>();
+            services.AddTransient<IVisitService, VisitService>();
+            services.AddTransient<ITestResultService, TestResultService>();
+            services.AddTransient<IMessagingService, MessagingService>();
+
+            return services;
+        }
+    }
+}
