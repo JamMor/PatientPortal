fc923d497f86196af084abd3c1b0c95b9a5745c2
JamMor <JamMor@gmail.com>
2025-05-13 14:58:53 -0400

    feat: add About page and update navigation links

--- src/PatientPortal/Controllers/HomeController.cs
+++ src/PatientPortal/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace PatientPortal.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
