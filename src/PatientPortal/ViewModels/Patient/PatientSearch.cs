84e8d92bb563e1acfa0046fcb3a4e547aa923d69
JamMor <JamMor@gmail.com>
2025-09-25 13:45:37 -0400

    refactor: remove unused User model
    Remove unused User model that was never implemented.
    Move the PastDate validation attribute to a more appropriate location in
     the Shared folder, and slightly refactor for nullable reference types
     while keeping functionality.

--- src/PatientPortal/ViewModels/Patient/PatientSearch.cs
+++ src/PatientPortal/ViewModels/Patient/PatientSearch.cs
@@ -3,6 +3,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using PatientPortal.Shared.Validation;
 
 namespace PatientPortal.Models
 {
